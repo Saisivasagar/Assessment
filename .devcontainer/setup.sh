dcf724edb26c635606462fa28970bd2bc502352f
John Glossner <jglossner@rivier.edu>
2025-05-15 13:52:10 -0400

    convert from conda to uv

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,29 +1,12 @@
 #!/bin/bash
 
-echo "Creating assessment conda environment. This will take some time (minutes)"
+echo "Creating uv environment."
 
-# Ensure Conda is initialized for the current shell
-eval "$(conda shell.bash hook)"
-
-# Create the Conda environment
-conda env create -f conda_env.yml -n assessment 
-
-# Clean up unnecessary files
-conda clean -a --yes
-
-# Initialize bashrc
-conda init
-
-# display environments
-conda info --envs
-
-# Add 'conda activate assessment' to ~/.bashrc if not already present
-if ! grep -q "conda activate assessment" ~/.bashrc; then
-    echo "Adding 'conda activate assessment' to ~/.bashrc"
-    echo -e "\n# Activate the assessment conda environment by default\nconda activate assessment" >> ~/.bashrc
-fi
-
-# Activate the environment
-conda activate assessment    
+# install uv and venv
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv venv --python 3.12
+source .venv/bin/activate
+uv pip install -r requirements.txt
 
 echo "Setup completed successfully!"
